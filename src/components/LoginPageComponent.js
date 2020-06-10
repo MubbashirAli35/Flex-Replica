60528b60751a35f53b9ea4f62e1159b9a0c213f6
Mubbashir Ali <mubbashirali35@gmail.com>
2020-06-11 03:10:32 +0500

    LoginPageComponent Form Added (1)

--- src/components/LoginPageComponent.js
+++ src/components/LoginPageComponent.js
@@ -6,13 +6,40 @@ function LoginPage(props) {
         <div classname='container'>
             <div className='row'>
                 
-                <div className='col-sm-5 offset-1'>
-                    <div className="logo">
-                        <img src={require('./images/flex-logo.png')} alt='logo' />
+                <div className='col-6'>
+                    <div style={{ marginLeft: '20%'}}>
+                        <div className="logo">
+                            <img src={require('./images/flex-logo.png')} alt='logo' />
+                        </div>
+                        <h2 className='font-styles' style={{ marginTop: '7%', marginLeft: '25%' }} >Sign In</h2>
+                        <div className='container'>
+                            <div className='row'>
+                                <div className='col-8'>
+                                    <Form>
+                                        <FormGroup>
+                                            <Label for='roll_no' style={{ fontFamily: 'Times New Roman'}}>Roll No.</Label>
+                                            <Input type='roll_no' name='roll_no' id='roll_no' 
+                                                placeholder='Roll Number i.e (17I-1234)' />
+                                        </FormGroup>
+                                        <FormGroup>
+                                            <Label for='password' style={{ fontFamily: 'Times New Roman'}}>Password</Label>
+                                            <Input type='password' name='password' id='password' 
+                                                placeholder='Password' />
+                                        </FormGroup>
+                                        <FormGroup check>
+                                            <Label check style={{ fontFamily: 'Times New Roman', marginBottom: '1%' }}>
+                                                <Input type='checkbox' />
+                                                Remember Me
+                                            </Label>
+                                        </FormGroup>
+                                    </Form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div className='col-sm-6'>
+                <div className='col-6'>
                     <img src={require('./images/login-page-bg-image.jpg')} 
                         alt='background' className='bg-image' />
                         <h2 className='heading-over-img'>Welcome to Flex-Student</h2>
